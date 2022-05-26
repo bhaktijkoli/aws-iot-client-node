6b8f04ecef4b0091e4c867d566d0f52c8baac9d6
Bhaktij Koli <bhaktijkoli121@gmail.com>
2022-05-27 01:59:53 +0530

    feat: attach certificate to thing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { config } from 'dotenv'
 import crypto from "crypto"
-import { CreateKeysAndCertificateCommand, CreateThingCommand, IoTClient } from "@aws-sdk/client-iot";
+import { AttachThingPrincipalCommand, CreateKeysAndCertificateCommand, CreateThingCommand, IoTClient } from "@aws-sdk/client-iot";
 import path from 'path';
 import fs from 'fs';
 
@@ -22,6 +22,7 @@ const awsIoTClient = new IoTClient({
     },
 });
 
+// Register Device
 const registerDevice = async () => {
 
     const command = new CreateThingCommand({
@@ -33,6 +34,7 @@ const registerDevice = async () => {
     console.log("Device Register");
 }
 
+// Create and Attach Certificate
 const createCertificate = async () => {
     const certsDir = path.join(__dirname, '..', 'certs')
     if (fs.existsSync(certsDir) === false) {
@@ -45,12 +47,12 @@ const createCertificate = async () => {
     if (fs.existsSync(certificatePemFile) || fs.existsSync(publicFile) || fs.existsSync(privateFile)) {
         return
     }
-
-    const command = new CreateKeysAndCertificateCommand({
+    // Create Certificate
+    const createCommand = new CreateKeysAndCertificateCommand({
         setAsActive: true
     })
-    const response = await awsIoTClient.send(command);
-    const { certificatePem, keyPair } = response
+    const createResponse = await awsIoTClient.send(createCommand);
+    const { certificatePem, keyPair } = createResponse
     if (!certificatePem || !keyPair) {
         return
     }
@@ -58,11 +60,19 @@ const createCertificate = async () => {
     fs.writeFileSync(certificatePemFile, String(certificatePem), 'utf-8')
     fs.writeFileSync(privateFile, String(PrivateKey), 'utf-8')
     fs.writeFileSync(publicFile, String(PublicKey), 'utf-8')
-    console.log({ response })
+    console.log({ createResponse })
+
+    // Attach Certificate to Thing
+    const attachCommand = new AttachThingPrincipalCommand({
+        principal: String(createResponse.certificateArn),
+        thingName: DEVICE_NAME
+    });
+    const attachResponse = await awsIoTClient.send(attachCommand);
+    console.log({ attachResponse });
 }
 
 const start = async () => {
-    // await registerDevice()
+    await registerDevice()
     await createCertificate()
     process.exit(0)
 }
